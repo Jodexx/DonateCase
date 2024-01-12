c818498731d4112adb2d3b09957acaa39af4cec9
Jodexx <0984134q@gmail.com>
2024-01-12 12:41:53 +0200

    2.1.7.7 subtitle index check

--- src/main/java/com/jodexindustries/donatecase/api/Case.java
+++ src/main/java/com/jodexindustries/donatecase/api/Case.java
@@ -398,9 +398,10 @@ public class Case {
                 String[] args = action.split(";");
                 String title = "";
                 String subTitle = "";
-                if(args.length == 1) {
+                if(args.length >= 1) {
                     title = args[0];
-                } else if(args.length > 1) {
+                }
+                if(args.length > 1) {
                     subTitle = args[1];
                 }
                 player.sendTitle(Main.t.rt(title, "%player%:" + player.getName(), "%group%:" + item.getGroup(), "%groupdisplayname%:" + item.getMaterial().getDisplayName()),
