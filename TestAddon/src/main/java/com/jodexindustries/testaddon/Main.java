e8b704e7ef737f8be4532da4075ef654e20c5f8e
Jodexx <0984134q@gmail.com>
2024-03-24 13:16:57 +0200

    2.1.9.9 new Test for CaseGuiClickEvent

--- TestAddon/src/main/java/com/jodexindustries/testaddon/Main.java
+++ TestAddon/src/main/java/com/jodexindustries/testaddon/Main.java
@@ -2,10 +2,18 @@ package com.jodexindustries.testaddon;
 
 import com.jodexindustries.donatecase.api.AnimationManager;
 import com.jodexindustries.donatecase.api.addon.JavaAddon;
+import com.jodexindustries.donatecase.api.events.CaseGuiClickEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
-public class Main extends JavaAddon {
+public class Main extends JavaAddon implements Listener {
     @Override
     public void onEnable() {
         AnimationManager.registerAnimation("test", new TestAnimation());
+        getDonateCase().getServer().getPluginManager().registerEvents(this, getDonateCase());
+    }
+    @EventHandler
+    public void onInventory(CaseGuiClickEvent e) {
+        getDonateCase().getServer().broadcastMessage(e.getCaseType());
     }
 }
