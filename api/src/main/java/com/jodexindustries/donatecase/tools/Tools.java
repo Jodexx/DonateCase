af9d8e372fd013e0add19784f87e937d46859a93
Jodexx <0984134q@gmail.com>
2024-04-23 19:50:13 +0300

    static tools

--- api/src/main/java/com/jodexindustries/donatecase/tools/Tools.java
+++ api/src/main/java/com/jodexindustries/donatecase/tools/Tools.java
@@ -1,13 +1,16 @@
 package com.jodexindustries.donatecase.tools;
 
-import com.jodexindustries.donatecase.api.armorstand.ArmorStandCreator;
 import com.jodexindustries.donatecase.api.armorstand.ArmorStandEulerAngle;
-import com.jodexindustries.donatecase.api.armorstand.BukkitArmorStandCreator;
-import com.jodexindustries.donatecase.api.armorstand.PacketArmorStandCreator;
-import com.jodexindustries.donatecase.api.data.CaseData;
 import com.jodexindustries.donatecase.api.data.MaterialType;
 import com.jodexindustries.donatecase.api.data.SubCommand;
 import com.jodexindustries.donatecase.api.data.SubCommandType;
+import com.jodexindustries.donatecase.api.armorstand.ArmorStandCreator;
+import com.jodexindustries.donatecase.api.armorstand.BukkitArmorStandCreator;
+import com.jodexindustries.donatecase.api.armorstand.PacketArmorStandCreator;
+import com.jodexindustries.donatecase.api.data.CaseData;
+import com.jodexindustries.donatecase.tools.support.CustomHeadSupport;
+import com.jodexindustries.donatecase.tools.support.HeadDatabaseSupport;
+import com.jodexindustries.donatecase.tools.support.ItemsAdderSupport;
 import org.bukkit.*;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.command.CommandSender;
@@ -58,31 +61,31 @@ public class Tools {
 
         return null;
     }
-    public ArmorStandCreator createArmorStand() {
+    public static ArmorStandCreator createArmorStand() {
         return createArmorStand();
     }
 
-    public void launchFirework(Location l) {}
+    public static void launchFirework(Location l) {}
 
-    public int c(int x, int y) {
+    public static int c(int x, int y) {
         int x2 = x - 1;
         int y2 = y - 1;
         return x2 + y2 * 9;
     }
 
-    public boolean isHere(Location l1, Location l2) {
+    public static boolean isHere(Location l1, Location l2) {
         return l1.getWorld() == l2.getWorld() && (int)l1.distance(l2) == 0;
     }
 
-    public void msg(CommandSender s, String msg) {}
+    public static void msg(CommandSender s, String msg) {}
 
-    public void msg_(CommandSender s, String msg) {
+    public static void msg_(CommandSender s, String msg) {
         if (s != null) {
             s.sendMessage(rc(msg));
         }
     }
 
-    public String rc(String t) {
+    public static String rc(String t) {
         if(t != null) {
             return hex(t);
         } else {
@@ -90,7 +93,7 @@ public class Tools {
         }
     }
 
-    public String rt(String text, String... repl) {
+    public static String rt(String text, String... repl) {
         for (String s : repl) {
             if(s != null) {
                 int l = s.split(":")[0].length();
@@ -105,7 +108,7 @@ public class Tools {
         return text;
     }
 
-    public List<String> rt(List<String> text, String... repl) {
+    public static List<String> rt(List<String> text, String... repl) {
         ArrayList<String> rt = new ArrayList<>();
 
         for (String t : text) {
@@ -114,13 +117,14 @@ public class Tools {
 
         return rt;
     }
-    public void convertCasesLocation() {}
+    public static void convertCasesLocation() {}
 
-    public void convertCases() {}
-    public List<File> getCasesInFolder() {
+    public static void convertCases() {
+    }
+    public static List<File> getCasesInFolder() {
         return getCasesInFolder();
     }
-    public Location fromString(String str) {
+    public static Location fromString(String str) {
         String regex = "Location\\{world=CraftWorld\\{name=(.*?)},x=(.*?),y=(.*?),z=(.*?),pitch=(.*?),yaw=(.*?)}";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(str);
@@ -142,7 +146,7 @@ public class Tools {
         return null;
     }
 
-    public List<String> rc(List<String> t) {
+    public static List<String> rc(List<String> t) {
         ArrayList<String> a = new ArrayList<>();
 
         for (String s : t) {
@@ -152,7 +156,7 @@ public class Tools {
         return a;
     }
 
-    public String getLocalPlaceholder(String string) {
+    public static String getLocalPlaceholder(String string) {
         Pattern pattern = Pattern.compile("%(.*?)%");
         Matcher matcher = pattern.matcher(string);
         if (matcher.find()) {
@@ -164,11 +168,11 @@ public class Tools {
         }
     }
 
-    public ItemStack createItem(Material ma, int amount, int data, String dn, boolean enchant, String[] rgb) {
+    public static ItemStack createItem(Material ma, int amount, int data, String dn, boolean enchant, String[] rgb) {
         return createItem(ma, data, amount, dn, null, enchant, rgb, -1);
     }
 
-    public Color parseColor(String s) {
+    public static Color parseColor(String s) {
 
         Color color = null;
         String[] split = s.split(" ");
@@ -222,7 +226,7 @@ public class Tools {
         return color;
     }
 
-    public ItemStack getPlayerHead(String player, String displayName, List<String> lore) {
+    public static ItemStack getPlayerHead(String player, String displayName, List<String> lore) {
         Material type = Material.getMaterial("SKULL_ITEM");
         ItemStack item;
         if (type == null) {
@@ -242,19 +246,68 @@ public class Tools {
 
         return item;
     }
-    public ItemStack getBASE64Skull(String url, String displayName, List<String> lore) {
-        return getBASE64Skull(url, displayName, lore);
+    public static ItemStack getBASE64Skull(String url, String displayName, List<String> lore) {
+        return getBASE64Skull(url,displayName,lore);
     }
-    public ItemStack getBASE64Skull(String url, String displayName) {
+    public static ItemStack getBASE64Skull(String url, String displayName) {
         return getBASE64Skull(url, displayName, null);
     }
 
     @NotNull
-    public ItemStack getCaseItem(String displayName, String id, boolean enchanted, String[] rgb) {
-        return getCaseItem(displayName,id,enchanted,rgb);
+    public static ItemStack getCaseItem(String displayName, String id, boolean enchanted, String[] rgb) {
+        MaterialType materialType = getMaterialType(id);
+        Material material;
+        ItemStack winItem;
+        if (!id.contains(":")) {
+            material = Material.getMaterial(id);
+            if (material == null) {
+                material = Material.STONE;
+            }
+            if(material != Material.AIR) {
+                winItem = createItem(material, 1, -1, displayName, enchanted, rgb);
+            } else {
+                winItem = new ItemStack(Material.AIR);
+            }
+        } else {
+            if (materialType == MaterialType.HEAD) {
+                String[] parts = id.split(":");
+                winItem = getPlayerHead(parts[1], displayName, null);
+            } else if (materialType == MaterialType.HDB) {
+                String[] parts = id.split(":");
+                String skullId = parts[1];
+                winItem = HeadDatabaseSupport.getSkull(skullId, displayName, null);
+            } else if (materialType == MaterialType.CH) {
+                String[] parts = id.split(":");
+                String category = parts[1];
+                String skullId = parts[2];
+                winItem = CustomHeadSupport.getSkull(category, skullId, displayName, null);
+            } else if (materialType == MaterialType.IA) {
+                String[] parts = id.split(":");
+                String namespace = parts[1];
+                String skullId = parts[2];
+                winItem = ItemsAdderSupport.getItem(namespace + ":" + skullId, displayName, null);
+            }
+            else if (materialType == MaterialType.BASE64) {
+                String[] parts = id.split(":");
+                String base64 = parts[1];
+                winItem = getBASE64Skull(base64, displayName);
+            } else {
+                String[] parts = id.split(":");
+                byte data = -1;
+                if(parts[1] != null) {
+                    data = Byte.parseByte(parts[1]);
+                }
+                material = Material.getMaterial(parts[0]);
+                if (material == null) {
+                    material = Material.STONE;
+                }
+                winItem = createItem(material, data, 1, displayName, enchanted, rgb);
+            }
+        }
+        return winItem;
     }
 
-    public ItemStack createItem(Material ma, int data, int amount, String dn, List<String> lore, boolean enchant, String[] rgb, int modelData) {
+    public static ItemStack createItem(Material ma, int data, int amount, String dn, List<String> lore, boolean enchant, String[] rgb, int modelData) {
         ItemStack item;
         if(data == -1) {
             item = new ItemStack(ma, amount);
@@ -274,7 +327,7 @@ public class Tools {
 
             if (lore != null) {
 
-                m.setLore(this.rc(lore));
+                m.setLore(rc(lore));
             }
             if(modelData != -1) {
                 m.setCustomModelData(modelData);
@@ -299,7 +352,7 @@ public class Tools {
         }
         return item;
     }
-    public MaterialType getMaterialType(String material) {
+    public static MaterialType getMaterialType(String material) {
         if (material.contains(":")) {
             String prefix = material.substring(0, material.indexOf(":"));
             switch (prefix) {
@@ -320,14 +373,14 @@ public class Tools {
         return MaterialType.DEFAULT;
     }
 
-    public List<Integer> getOpenMaterialSlots(String c) {
+    public static List<Integer> getOpenMaterialSlots(String c) {
         return getOpenMaterialSlots(c);
     }
-    public Map<List<Integer>, String> getOpenMaterialItemsBySlots(String c) {
-    return getOpenMaterialItemsBySlots(c);
+    public static Map<List<Integer>, String> getOpenMaterialItemsBySlots(String c) {
+        return getOpenMaterialItemsBySlots(c);
     }
 
-    public String getOpenMaterialTypeByMapBySlot(String c, int slot) {
+    public static String getOpenMaterialTypeByMapBySlot(String c, int slot) {
         for (Map.Entry<List<Integer>, String> entry : getOpenMaterialItemsBySlots(c).entrySet()) {
             if(entry.getKey().contains(slot)) {
                 return entry.getValue();
@@ -337,7 +390,7 @@ public class Tools {
     }
 
 
-    public String hex(String message) {
+    public static String hex(String message) {
         Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
         Matcher matcher = pattern.matcher(message);
         while (matcher.find()) {
@@ -356,7 +409,7 @@ public class Tools {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 
-    public int getPluginVersion(String version) {
+    public static int getPluginVersion(String version) {
         StringBuilder builder = new StringBuilder();
         version = version.replaceAll("\\.", "");
         if(version.length() < 4) {
@@ -366,7 +419,7 @@ public class Tools {
         }
         return Integer.parseInt(builder.toString());
     }
-    public boolean isHasCommandForSender(CommandSender sender, Map<String, List<Map<String, SubCommand>>> addonsMap) {
+    public static boolean isHasCommandForSender(CommandSender sender, Map<String, List<Map<String, SubCommand>>> addonsMap) {
         for (String addon : addonsMap.keySet()) {
             List<Map<String, SubCommand>> commands = addonsMap.get(addon);
             for (Map<String, SubCommand> command : commands) {
@@ -390,29 +443,29 @@ public class Tools {
         }
         return false;
     }
-    public boolean isHasCommandForSender(CommandSender sender, Map<String, List<Map<String, SubCommand>>> addonsMap, String addon) {
-            List<Map<String, SubCommand>> commands = addonsMap.get(addon);
-            for (Map<String, SubCommand> command : commands) {
-                for (String commandName : command.keySet()) {
-                    SubCommand subCommand = command.get(commandName);
-                    if (sender.hasPermission("donatecase.admin")) {
-                        if (subCommand.getType() == SubCommandType.ADMIN || subCommand.getType() == SubCommandType.MODER || subCommand.getType() == SubCommandType.PLAYER || subCommand.getType() == null) {
-                            return true;
-                        }
-                    } else if (sender.hasPermission("donatecase.mod") && !sender.hasPermission("donatecase.admin")) {
-                        if (subCommand.getType() == SubCommandType.MODER || subCommand.getType() == SubCommandType.PLAYER || subCommand.getType() == null) {
-                            return true;
-                        }
-                    } else if (sender.hasPermission("donatecase.player") && !sender.hasPermission("donatecase.admin") && !sender.hasPermission("donatecase.mod")) {
-                        if (subCommand.getType() == SubCommandType.PLAYER || subCommand.getType() == null) {
-                            return true;
-                        }
+    public static boolean isHasCommandForSender(CommandSender sender, Map<String, List<Map<String, SubCommand>>> addonsMap, String addon) {
+        List<Map<String, SubCommand>> commands = addonsMap.get(addon);
+        for (Map<String, SubCommand> command : commands) {
+            for (String commandName : command.keySet()) {
+                SubCommand subCommand = command.get(commandName);
+                if (sender.hasPermission("donatecase.admin")) {
+                    if (subCommand.getType() == SubCommandType.ADMIN || subCommand.getType() == SubCommandType.MODER || subCommand.getType() == SubCommandType.PLAYER || subCommand.getType() == null) {
+                        return true;
+                    }
+                } else if (sender.hasPermission("donatecase.mod") && !sender.hasPermission("donatecase.admin")) {
+                    if (subCommand.getType() == SubCommandType.MODER || subCommand.getType() == SubCommandType.PLAYER || subCommand.getType() == null) {
+                        return true;
+                    }
+                } else if (sender.hasPermission("donatecase.player") && !sender.hasPermission("donatecase.admin") && !sender.hasPermission("donatecase.mod")) {
+                    if (subCommand.getType() == SubCommandType.PLAYER || subCommand.getType() == null) {
+                        return true;
                     }
                 }
             }
+        }
         return false;
     }
-    public EulerAngle getEulerAngleFromString(String angleString) {
+    public static EulerAngle getEulerAngleFromString(String angleString) {
         String[] angle;
         if (angleString == null) return new EulerAngle(0,0,0);
         angle = angleString.replace(" ", "").split(",");
@@ -425,8 +478,8 @@ public class Tools {
             return new EulerAngle(0,0,0);
         }
     }
-    public ArmorStandEulerAngle getArmorStandEulerAngle(String path) {
-         return getArmorStandEulerAngle(path);
+    public static ArmorStandEulerAngle getArmorStandEulerAngle(String path) {
+        return getArmorStandEulerAngle(path);
     }
 
 }
