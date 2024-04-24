1c7a8787aeac915e518e3a1c483c9d011c81a352
Jodexx <0984134q@gmail.com>
2024-04-24 12:27:50 +0300

    added check for inventory size

--- src/main/java/com/jodexindustries/donatecase/gui/CaseGui.java
+++ src/main/java/com/jodexindustries/donatecase/gui/CaseGui.java
@@ -23,135 +23,143 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static com.jodexindustries.donatecase.DonateCase.*;
+import static com.jodexindustries.donatecase.api.Case.playersGui;
 
 public class CaseGui {
-    private final Inventory inventory;
+    private Inventory inventory;
 
     public CaseGui(Player p, CaseData caseData) {
         String title = caseData.getCaseTitle();
         String c = caseData.getCaseName();
         YamlConfiguration configCase = casesConfig.getCase(c);
-        inventory = Bukkit.createInventory(null, configCase.getInt("case.Gui.Size", 45), Tools.rc(title));
-        ConfigurationSection items = configCase.getConfigurationSection("case.Gui.Items");
-        Bukkit.getScheduler().runTaskAsynchronously(instance, () -> {
-            List<CaseData.HistoryData> globalHistoryData = Case.getSortedHistoryData();
-            if (items != null) {
-            for (String item : items.getKeys(false)) {
-                String material = configCase.getString("case.Gui.Items." + item + ".Material", "STONE");
-                String displayName = configCase.getString("case.Gui.Items." + item + ".DisplayName", "None");
-                int keys;
-                String placeholder = Tools.getLocalPlaceholder(displayName);
-                if (placeholder.startsWith("keys_")) {
-                    String[] parts = placeholder.split("_");
-                    String caseName = parts[1];
-                    keys = Case.getKeys(caseName, p.getName());
-                } else {
-                    keys = Case.getKeys(c, p.getName());
-                }
-                displayName.replace("%" + placeholder + "%", String.valueOf(keys));
-                if(instance.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-                    displayName = PAPISupport.setPlaceholders(p, displayName);
-                }
-                boolean enchanted = configCase.getBoolean("case.Gui.Items." + item + ".Enchanted");
-                String itemType = configCase.getString("case.Gui.Items." + item + ".Type", "DEFAULT");
-                List<String> lore = Tools.rc(configCase.getStringList("case.Gui.Items." + item + ".Lore"));
-                int modelData = configCase.getInt("case.Gui.Items." + item + ".ModelData", -1);
-                String[] rgb = null;
-                List<String> pLore = new ArrayList<>();
-                for(String line : lore) {
-                    if(instance.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-                        line = PAPISupport.setPlaceholders(p, line);
-                    }
-                    pLore.add(line);
-                }
-                lore = Tools.rc(pLore);
-                if (material.toUpperCase().startsWith("LEATHER_")) {
-                    String rgbString = configCase.getString("case.Gui.Items." + item + ".Rgb");
-                    if (rgbString != null) {
-                        rgb = rgbString.replace(" ", "").split(",");
-                    }
-                }
-                if (itemType.startsWith("HISTORY")) {
-                    String[] typeArgs = itemType.split("-");
-                    int index = Integer.parseInt(typeArgs[1]);
-                    String caseType = (typeArgs.length >= 3) ? typeArgs[2] : c;
-                    boolean isGlobal = caseType.equalsIgnoreCase("GLOBAL");
-
-                    CaseData historyCaseData = isGlobal ? null : Case.getCase(caseType);
-                    if (historyCaseData == null && !isGlobal) {
-                        instance.getLogger().warning("Case " + caseType + " HistoryData is null!");
-                        continue;
-                    }
-                    if (!isGlobal) {
-                        historyCaseData = historyCaseData.clone();
-                    }
-
-                    CaseData.HistoryData data = null;
-                    if (isGlobal) {
-                        if(globalHistoryData.size() <= index) continue;
-                        data = globalHistoryData.get(index);
-                    } else {
-                        if(!sql) {
-                            data = historyCaseData.getHistoryData()[index];
+        int size = configCase.getInt("case.Gui.Size", 45);
+        if(size >= 9 && size <= 54 && size % 9 == 0) {
+            inventory = Bukkit.createInventory(null, configCase.getInt("case.Gui.Size", 45), Tools.rc(title));
+            ConfigurationSection items = configCase.getConfigurationSection("case.Gui.Items");
+            Bukkit.getScheduler().runTaskAsynchronously(instance, () -> {
+                List<CaseData.HistoryData> globalHistoryData = Case.getSortedHistoryData();
+                if (items != null) {
+                    for (String item : items.getKeys(false)) {
+                        String material = configCase.getString("case.Gui.Items." + item + ".Material", "STONE");
+                        String displayName = configCase.getString("case.Gui.Items." + item + ".DisplayName", "None");
+                        int keys;
+                        String placeholder = Tools.getLocalPlaceholder(displayName);
+                        if (placeholder.startsWith("keys_")) {
+                            String[] parts = placeholder.split("_");
+                            String caseName = parts[1];
+                            keys = Case.getKeys(caseName, p.getName());
                         } else {
-                            List<CaseData.HistoryData> dbData = Case.sortHistoryDataByCase(globalHistoryData,caseType);
-                            if(!dbData.isEmpty()) {
-                                if(dbData.size() <= index) continue;
-                                data = dbData.get(index);
+                            keys = Case.getKeys(c, p.getName());
+                        }
+                        displayName.replace("%" + placeholder + "%", String.valueOf(keys));
+                        if (instance.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+                            displayName = PAPISupport.setPlaceholders(p, displayName);
+                        }
+                        boolean enchanted = configCase.getBoolean("case.Gui.Items." + item + ".Enchanted");
+                        String itemType = configCase.getString("case.Gui.Items." + item + ".Type", "DEFAULT");
+                        List<String> lore = Tools.rc(configCase.getStringList("case.Gui.Items." + item + ".Lore"));
+                        int modelData = configCase.getInt("case.Gui.Items." + item + ".ModelData", -1);
+                        String[] rgb = null;
+                        List<String> pLore = new ArrayList<>();
+                        for (String line : lore) {
+                            if (instance.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+                                line = PAPISupport.setPlaceholders(p, line);
+                            }
+                            pLore.add(line);
+                        }
+                        lore = Tools.rc(pLore);
+                        if (material.toUpperCase().startsWith("LEATHER_")) {
+                            String rgbString = configCase.getString("case.Gui.Items." + item + ".Rgb");
+                            if (rgbString != null) {
+                                rgb = rgbString.replace(" ", "").split(",");
                             }
                         }
-                    }
-                    if (data == null) continue;
-                    if (isGlobal) historyCaseData = Case.getCase(data.getCaseType());
-                    if(historyCaseData == null) continue;
+                        if (itemType.startsWith("HISTORY")) {
+                            String[] typeArgs = itemType.split("-");
+                            int index = Integer.parseInt(typeArgs[1]);
+                            String caseType = (typeArgs.length >= 3) ? typeArgs[2] : c;
+                            boolean isGlobal = caseType.equalsIgnoreCase("GLOBAL");
 
-                    CaseData.Item historyItem = historyCaseData.getItem(data.getItem());
-                    if(historyItem == null) continue;
-                    material = configCase.getString("case.Gui.Items." + item + ".Material", "HEAD:" + data.getPlayerName());
-                    if(material.equalsIgnoreCase("DEFAULT")) {
-                        material = historyItem.getMaterial().getId();
-                    }
-                    CaseData.Item.RandomAction randomAction = historyItem.getRandomAction(data.getAction());
-                    String randomActionDisplayname = randomAction != null ? randomAction.getDisplayName() : "";
-                    DateFormat formatter = new SimpleDateFormat(customConfig.getConfig().getString("DonatCase.DateFormat", "dd.MM HH:mm:ss"));
-                    String dateFormatted = formatter.format(new Date(data.getTime()));
-                    String groupDisplayName = data.getItem() != null ? historyItem.getMaterial().getDisplayName() : "open_case_again";
-                    String[] template = {"%action%:" + data.getAction(), "%actiondisplayname%:" + randomActionDisplayname, "%casedisplayname%:" + historyCaseData.getCaseDisplayName(), "%casename%:" + data.getCaseType(), "%casetitle%:" + historyCaseData.getCaseTitle(), "%time%:" + dateFormatted, "%group%:" + data.getGroup(), "%player%:" + data.getPlayerName(), "%groupdisplayname%:" + groupDisplayName};
-                    displayName = Tools.rt(displayName, template);
-                    lore = Tools.rt(lore, template);
-                }
+                            CaseData historyCaseData = isGlobal ? null : Case.getCase(caseType);
+                            if (historyCaseData == null && !isGlobal) {
+                                instance.getLogger().warning("Case " + caseType + " HistoryData is null!");
+                                continue;
+                            }
+                            if (!isGlobal) {
+                                historyCaseData = historyCaseData.clone();
+                            }
 
-                List<String> slots = new ArrayList<>();
-                if (configCase.isList("case.Gui.Items." + item + ".Slots")) {
-                    List<String> temp = configCase.getStringList("case.Gui.Items." + item + ".Slots");
-                    for (String slot : temp) {
-                        String[] values = slot.split("-", 2);
-                        if (values.length == 2) {
-                            for (int i = Integer.parseInt(values[0]); i <= Integer.parseInt(values[1]); i++) {
-                                slots.add(String.valueOf(i));
+                            CaseData.HistoryData data = null;
+                            if (isGlobal) {
+                                if (globalHistoryData.size() <= index) continue;
+                                data = globalHistoryData.get(index);
+                            } else {
+                                if (!sql) {
+                                    data = historyCaseData.getHistoryData()[index];
+                                } else {
+                                    List<CaseData.HistoryData> dbData = Case.sortHistoryDataByCase(globalHistoryData, caseType);
+                                    if (!dbData.isEmpty()) {
+                                        if (dbData.size() <= index) continue;
+                                        data = dbData.get(index);
+                                    }
+                                }
+                            }
+                            if (data == null) continue;
+                            if (isGlobal) historyCaseData = Case.getCase(data.getCaseType());
+                            if (historyCaseData == null) continue;
+
+                            CaseData.Item historyItem = historyCaseData.getItem(data.getItem());
+                            if (historyItem == null) continue;
+                            material = configCase.getString("case.Gui.Items." + item + ".Material", "HEAD:" + data.getPlayerName());
+                            if (material.equalsIgnoreCase("DEFAULT")) {
+                                material = historyItem.getMaterial().getId();
+                            }
+                            CaseData.Item.RandomAction randomAction = historyItem.getRandomAction(data.getAction());
+                            String randomActionDisplayname = randomAction != null ? randomAction.getDisplayName() : "";
+                            DateFormat formatter = new SimpleDateFormat(customConfig.getConfig().getString("DonatCase.DateFormat", "dd.MM HH:mm:ss"));
+                            String dateFormatted = formatter.format(new Date(data.getTime()));
+                            String groupDisplayName = data.getItem() != null ? historyItem.getMaterial().getDisplayName() : "open_case_again";
+                            String[] template = {"%action%:" + data.getAction(), "%actiondisplayname%:" + randomActionDisplayname, "%casedisplayname%:" + historyCaseData.getCaseDisplayName(), "%casename%:" + data.getCaseType(), "%casetitle%:" + historyCaseData.getCaseTitle(), "%time%:" + dateFormatted, "%group%:" + data.getGroup(), "%player%:" + data.getPlayerName(), "%groupdisplayname%:" + groupDisplayName};
+                            displayName = Tools.rt(displayName, template);
+                            lore = Tools.rt(lore, template);
+                        }
+
+                        List<String> slots = new ArrayList<>();
+                        if (configCase.isList("case.Gui.Items." + item + ".Slots")) {
+                            List<String> temp = configCase.getStringList("case.Gui.Items." + item + ".Slots");
+                            for (String slot : temp) {
+                                String[] values = slot.split("-", 2);
+                                if (values.length == 2) {
+                                    for (int i = Integer.parseInt(values[0]); i <= Integer.parseInt(values[1]); i++) {
+                                        slots.add(String.valueOf(i));
+                                    }
+                                } else {
+                                    slots.add(slot);
+                                }
                             }
                         } else {
-                            slots.add(slot);
+                            String[] slotArgs = configCase.getString("case.Gui.Items." + item + ".Slots", "0-0").split("-");
+                            int range1 = Integer.parseInt(slotArgs[0]);
+                            int range2 = range1;
+                            if (slotArgs.length >= 2) {
+                                range2 = Integer.parseInt(slotArgs[1]);
+                            }
+                            slots.addAll(IntStream.rangeClosed(range1, range2).mapToObj(String::valueOf).collect(Collectors.toList()));
+                        }
+
+                        ItemStack itemStack = getItem(material, displayName, lore, c, p, enchanted, rgb, modelData);
+                        for (String slot : slots) {
+                            inventory.setItem(Integer.parseInt(slot), itemStack);
                         }
                     }
-                } else {
-                    String[] slotArgs = configCase.getString("case.Gui.Items." + item + ".Slots", "0-0").split("-");
-                    int range1 = Integer.parseInt(slotArgs[0]);
-                    int range2 = range1;
-                    if(slotArgs.length >= 2) {
-                        range2 = Integer.parseInt(slotArgs[1]);
-                    }
-                    slots.addAll(IntStream.rangeClosed(range1, range2).mapToObj(String::valueOf).collect(Collectors.toList()));
                 }
-
-                ItemStack itemStack = getItem(material, displayName, lore, c, p, enchanted, rgb, modelData);
-                for (String slot : slots) {
-                    inventory.setItem(Integer.parseInt(slot), itemStack);
-                }
-            }
+            });
+            p.openInventory(inventory);
+        } else {
+            Tools.msg(p, "&cSomething wrong! Contact with server administrator!");
+            instance.getLogger().warning("Wrong GUI size! Use: 9, 18, 27, 36, 45, 54");
+            playersGui.remove(p.getUniqueId());
         }
-        });
-        p.openInventory(inventory);
     }
     public Inventory getInventory() {
         return inventory;
