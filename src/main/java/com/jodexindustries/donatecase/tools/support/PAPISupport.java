621da8d0c4e3a731f54391edfb65398563c75d45
Jodexx <0984134q@gmail.com>
2024-05-26 11:39:37 +0300

    Set-up OfflinePlayer placeholders

--- src/main/java/com/jodexindustries/donatecase/tools/support/PAPISupport.java
+++ src/main/java/com/jodexindustries/donatecase/tools/support/PAPISupport.java
@@ -1,12 +1,12 @@
 package com.jodexindustries.donatecase.tools.support;
 
 import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
 
 import static com.jodexindustries.donatecase.DonateCase.instance;
 
 public class PAPISupport {
-    public static String setPlaceholders(Player player, String text) {
+    public static String setPlaceholders(OfflinePlayer player, String text) {
         if(instance.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             text = PlaceholderAPI.setPlaceholders(player, text);
         }
