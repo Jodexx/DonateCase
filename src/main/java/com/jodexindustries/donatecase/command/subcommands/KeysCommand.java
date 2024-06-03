f2e17324c40bb8bff0a3480bedf39c7ae7b1d7c1
Jodexx <0984134q@gmail.com>
2024-06-03 19:52:46 +0300

    Moved KeysCommand logic to external class

---
+++ src/main/java/com/jodexindustries/donatecase/command/subcommands/KeysCommand.java
@@ -0,0 +1,90 @@
+package com.jodexindustries.donatecase.command.subcommands;
+
+import com.jodexindustries.donatecase.api.Case;
+import com.jodexindustries.donatecase.api.data.SubCommand;
+import com.jodexindustries.donatecase.api.data.SubCommandType;
+import com.jodexindustries.donatecase.tools.Tools;
+import com.jodexindustries.donatecase.tools.support.PAPISupport;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.jodexindustries.donatecase.DonateCase.customConfig;
+import static com.jodexindustries.donatecase.DonateCase.instance;
+
+public class KeysCommand implements SubCommand {
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (args.length < 1) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (sender.hasPermission("donatecase.player")) {
+                    for (String string : customConfig.getLang().getStringList("MyKeys")) {
+                        if(instance.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+                            string = PAPISupport.setPlaceholders(player, string);
+                        }
+                        String placeholder = Tools.getLocalPlaceholder(string);
+                        String result = "0";
+                        if(placeholder.startsWith("keys_")) {
+                            String[] parts = placeholder.split("_");
+                            String caseTitle = parts[1];
+                            int keys = Case.getKeys(caseTitle, player.getName());
+                            if(parts.length == 2) {
+                                result = String.valueOf(keys);
+                            } else if (parts.length == 3 && parts[2].equalsIgnoreCase("format")) {
+                                result = NumberFormat.getNumberInstance().format(keys);
+                            }
+                        }
+                        sender.sendMessage(Tools.rc(string.replace("%" + placeholder + "%", result)));
+                    }
+                }
+            }
+        } else {
+            if (sender.hasPermission("donatecase.mod")) {
+                String target = args[0];
+                //Get player keys
+                for (String string : customConfig.getLang().getStringList("PlayerKeys")) {
+                    if(instance.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+                        Player targetPlayer = Bukkit.getPlayerExact(target);
+                        if(targetPlayer != null) {
+                            string = PAPISupport.setPlaceholders(targetPlayer, string);
+                        }
+                    }
+                    String placeholder = Tools.getLocalPlaceholder(string);
+                    String result = "0";
+                    if(placeholder.startsWith("keys_")) {
+                        String[] parts = placeholder.split("_");
+                        String caseName = parts[1];
+                        int keys = Case.getKeys(caseName, target);
+                        if(parts.length == 2) {
+                            result = String.valueOf(keys);
+                        } else if (parts.length == 3 && parts[2].equalsIgnoreCase("format")) {
+                            result = NumberFormat.getNumberInstance().format(keys);
+                        }
+                    }
+                    sender.sendMessage(Tools.rc(string).replace("%player", target)
+                            .replace("%"+placeholder+"%", result));
+                }
+            } else {
+                Tools.msg_(sender, Tools.rt(customConfig.getLang().getString("NoPermission")));
+            }
+        }
+    }
+
+    @Override
+    public List<String> getTabCompletions(CommandSender sender, String[] args) {
+        if (args.length != 1 || !sender.hasPermission("donatecase.mod")) {
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(Bukkit.getOnlinePlayers().stream().map(Player::getName).filter(px -> px.startsWith(args[0])).collect(Collectors.toList()));    }
+
+    @Override
+    public SubCommandType getType() {
+        return SubCommandType.PLAYER;
+    }
+}
