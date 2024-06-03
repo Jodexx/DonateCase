e557209973be1b0ac86e9a87198bf17743e5f19f
Jodexx <0984134q@gmail.com>
2024-06-03 19:52:19 +0300

    Moved HelpCommand logic to external class

---
+++ src/main/java/com/jodexindustries/donatecase/command/subcommands/HelpCommand.java
@@ -0,0 +1,26 @@
+package com.jodexindustries.donatecase.command.subcommands;
+
+import com.jodexindustries.donatecase.api.data.SubCommand;
+import com.jodexindustries.donatecase.api.data.SubCommandType;
+import com.jodexindustries.donatecase.command.GlobalCommand;
+import org.bukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelpCommand implements SubCommand {
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        GlobalCommand.sendHelp(sender, "dc");
+    }
+
+    @Override
+    public List<String> getTabCompletions(CommandSender sender, String[] args) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public SubCommandType getType() {
+        return SubCommandType.PLAYER;
+    }
+}
