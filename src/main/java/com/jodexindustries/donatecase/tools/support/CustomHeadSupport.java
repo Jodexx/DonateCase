ef00451b3bc5d87b4498a3446dc2598832a331c6
Jodexx <0984134q@gmail.com>
2024-06-03 15:10:22 +0300

    Refactor CH support class

--- src/main/java/com/jodexindustries/donatecase/tools/support/CustomHeadSupport.java
+++ src/main/java/com/jodexindustries/donatecase/tools/support/CustomHeadSupport.java
@@ -15,9 +15,9 @@ import static com.jodexindustries.donatecase.DonateCase.instance;
 
 public class CustomHeadSupport {
     public static ItemStack getSkull(@NotNull String category, String id, String displayName, List<String> lore) {
+        ItemStack item = new ItemStack(Material.STONE);
         if(instance.getServer().getPluginManager().isPluginEnabled("CustomHeads")) {
             CustomHeadsAPI api = CustomHeads.getApi();
-            ItemStack item = new ItemStack(Material.STONE);
             try {
                 item = api.getHead(category, Integer.parseInt(id));
             } catch (NullPointerException nullPointerException) {
@@ -33,10 +33,9 @@ public class CustomHeadSupport {
                 }
             }
             item.setItemMeta(itemMeta);
-            return item;
         } else {
             Logger.log("&eYou're using an item from CustomHeads, but it's not loaded on the server!");
         }
-        return new ItemStack(Material.STONE);
+        return item;
     }
 }
