c74bff5f0270cff3c60bcbb75d4a0c8c436bab5e
Jodexx <0984134q@gmail.com>
2024-06-03 15:09:33 +0300

    Refactor HDB support class

--- src/main/java/com/jodexindustries/donatecase/tools/support/HeadDatabaseSupport.java
+++ src/main/java/com/jodexindustries/donatecase/tools/support/HeadDatabaseSupport.java
@@ -13,9 +13,9 @@ import static com.jodexindustries.donatecase.DonateCase.instance;
 
 public class HeadDatabaseSupport {
     public static ItemStack getSkull(String id, String displayName, List<String> lore) {
+        ItemStack item = new ItemStack(Material.STONE);
         if (instance.getServer().getPluginManager().isPluginEnabled("HeadDataBase")) {
             HeadDatabaseAPI api = new HeadDatabaseAPI();
-            ItemStack item = new ItemStack(Material.STONE);
             try {
                 item = api.getItemHead(id);
             } catch (NullPointerException nullPointerException) {
@@ -31,10 +31,9 @@ public class HeadDatabaseSupport {
                 }
                 item.setItemMeta(itemMeta);
             }
-            return item;
         } else {
             Logger.log("&eYou're using an head from HeadDataBase, but it's not loaded on the server!");
         }
-        return new ItemStack(Material.STONE);
+        return item;
     }
 }
