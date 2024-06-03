19851184a4fa0d7c5f86b835b038c0e1eecc75fe
Jodexx <0984134q@gmail.com>
2024-06-03 15:14:41 +0300

    Refactor ItemsAdder support class

--- src/main/java/com/jodexindustries/donatecase/tools/support/ItemsAdderSupport.java
+++ src/main/java/com/jodexindustries/donatecase/tools/support/ItemsAdderSupport.java
@@ -3,6 +3,7 @@ package com.jodexindustries.donatecase.tools.support;
 import com.jodexindustries.donatecase.tools.Logger;
 import com.jodexindustries.donatecase.tools.Tools;
 import dev.lone.itemsadder.api.CustomStack;
+import dev.lone.itemsadder.api.NotActuallyItemsAdderException;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -14,25 +15,29 @@ import static com.jodexindustries.donatecase.DonateCase.instance;
 
 public class ItemsAdderSupport {
     public static ItemStack getItem(@NotNull String namespace, String displayName, List<String> lore) {
+        ItemStack item = new ItemStack(Material.STONE);
         if(instance.getServer().getPluginManager().isPluginEnabled("ItemsAdder")) {
-            CustomStack stack = CustomStack.getInstance(namespace);
-            if (stack != null) {
-                ItemStack item = stack.getItemStack();
-                ItemMeta itemMeta = item.getItemMeta();
-                if(displayName != null) {
-                    itemMeta.setDisplayName(Tools.rc(displayName));
+            try {
+                CustomStack stack = CustomStack.getInstance(namespace);
+                if(stack != null) {
+                    item = stack.getItemStack();
+                    ItemMeta itemMeta = item.getItemMeta();
+                    if (displayName != null) {
+                        itemMeta.setDisplayName(Tools.rc(displayName));
+                    }
+                    if (lore != null) {
+                        itemMeta.setLore(Tools.rc(lore));
+                    }
+                    item.setItemMeta(itemMeta);
+                } else {
+                    Logger.log("&eCould not find the item you were looking for by ItemsAdder support. Namespace: " + namespace);
                 }
-                if (lore != null) {
-                    itemMeta.setLore(Tools.rc(lore));
-                }
-                item.setItemMeta(itemMeta);
-                return item;
-            } else {
-                Logger.log("&eCould not find the item you were looking for");
+            } catch (NotActuallyItemsAdderException ignored) {
+                Logger.log("&eCould not find the item you were looking for by ItemsAdder support. Namespace: " + namespace);
             }
-        } else {
+            } else {
             Logger.log("&eYou're using an item from ItemsAdder, but it's not loaded on the server!");
         }
-        return new ItemStack(Material.STONE);
+        return item;
     }
 }
