c4144c6bf43c0ef83b37104682e88be730e3132e
Jodexx <0984134q@gmail.com>
2024-06-16 14:36:13 +0300

    getDriver method

--- src/main/java/com/jodexindustries/donatecase/api/data/PermissionDriver.java
+++ src/main/java/com/jodexindustries/donatecase/api/data/PermissionDriver.java
@@ -1,6 +1,15 @@
 package com.jodexindustries.donatecase.api.data;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum PermissionDriver {
     luckperms,
-    vault
+    vault;
+    public static PermissionDriver getDriver(@NotNull String name) {
+        PermissionDriver result = null;
+        try {
+            result = PermissionDriver.valueOf(name.toLowerCase());
+        } catch (IllegalArgumentException ignored) {}
+        return result;
+    }
 }
