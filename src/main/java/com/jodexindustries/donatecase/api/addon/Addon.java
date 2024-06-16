48e48075b98f4ed7f85078b67ef23ccb146d2edf
Jodexx <0984134q@gmail.com>
2024-06-16 19:41:28 +0300

    comments

--- src/main/java/com/jodexindustries/donatecase/api/addon/Addon.java
+++ src/main/java/com/jodexindustries/donatecase/api/addon/Addon.java
@@ -1,7 +1,25 @@
 package com.jodexindustries.donatecase.api.addon;
 
+/**
+ * Represent an Addon
+ */
 public interface Addon {
+
+    /**
+     * Returns the name of the addon
+     */
     String getName();
+
+    /**
+     * An addon revision identifier
+     */
     String getVersion();
+
+    /**
+     * Returns a value indicating whether or not this addon is currently
+     * enabled
+     *
+     * @return true if this addon is enabled, otherwise false
+     */
     boolean isEnabled();
 }
