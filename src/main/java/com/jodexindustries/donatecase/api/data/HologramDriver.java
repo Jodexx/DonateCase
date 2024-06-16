8ed6021ed38ce15b8cee889298fe014624be7932
Jodexx <0984134q@gmail.com>
2024-06-16 14:36:31 +0300

    enum for HologramDriver selecting

---
+++ src/main/java/com/jodexindustries/donatecase/api/data/HologramDriver.java
@@ -0,0 +1,17 @@
+package com.jodexindustries.donatecase.api.data;
+
+import org.jetbrains.annotations.NotNull;
+
+public enum HologramDriver {
+    decentholograms,
+    holographicdisplays,
+    cmi;
+
+    public static HologramDriver getDriver(@NotNull String name) {
+        HologramDriver result = null;
+        try {
+            result = HologramDriver.valueOf(name.toLowerCase());
+        } catch (IllegalArgumentException ignored) {}
+        return result;
+    }
+}
