ee8f8ab76cf84b407cb59be8f86ad4558b191640
Jodexx <0984134q@gmail.com>
2024-06-24 17:30:20 +0300

    Add load GUI logic

--- src/main/java/com/jodexindustries/donatecase/tools/CaseLoader.java
+++ src/main/java/com/jodexindustries/donatecase/tools/CaseLoader.java
@@ -7,10 +7,14 @@ import org.bukkit.Sound;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CaseLoader {
    private final DonateCase plugin;
@@ -50,7 +54,9 @@ public class CaseLoader {
         CaseData.HistoryData[] historyData = loadHistoryData(caseType);
         Map<String, Integer> levelGroups = loadLevelGroups(caseSection);
 
-        return new CaseData(caseType, caseDisplayName, caseTitle, animationName, sound, items, historyData, hologram, levelGroups);
+        CaseData.GUI gui = loadGUI(caseSection);
+
+        return new CaseData(caseType, caseDisplayName, caseTitle, animationName, sound, items, historyData, hologram, levelGroups, gui);
     }
 
     private String getStringOrDefault(ConfigurationSection section, String path) {
@@ -112,7 +118,7 @@ public class CaseLoader {
         List<String> actions = itemSection.getStringList("Actions");
         List<String> alternativeActions = itemSection.getStringList("AlternativeActions");
         Map<String, CaseData.Item.RandomAction> randomActions = loadRandomActions(itemSection);
-        String[] rgb = loadRgb(itemSection);
+        String[] rgb = loadRgb(itemSection, "Item.Rgb");
         CaseData.Item.Material material = loadMaterial(itemSection);
 
         return new CaseData.Item(item, group, chance, material, giveType, actions, randomActions, rgb, alternativeActions);
@@ -140,8 +146,8 @@ public class CaseLoader {
         return randomActions;
     }
 
-    private String[] loadRgb(ConfigurationSection itemSection) {
-        String rgbString = itemSection.getString("Item.Rgb");
+    private String[] loadRgb(ConfigurationSection itemSection, String path) {
+        String rgbString = itemSection.getString(path);
         return rgbString == null ? null : rgbString.replaceAll(" ", "").split(",");
     }
 
@@ -149,9 +155,9 @@ public class CaseLoader {
         String id = itemSection.getString("Item.ID", "STONE");
         String itemDisplayName = Tools.rc(itemSection.getString("Item.DisplayName"));
         boolean enchanted = itemSection.getBoolean("Item.Enchanted");
-        ItemStack itemStack = Tools.getCaseItem(itemDisplayName, id, enchanted, loadRgb(itemSection));
+        ItemStack itemStack = Tools.getCaseItem(itemDisplayName, id, enchanted, loadRgb(itemSection, "Item.Rg"));
 
-        return new CaseData.Item.Material(id, itemStack, itemDisplayName, enchanted);
+        return new CaseData.Item.Material(id, itemStack, itemDisplayName, enchanted, null);
     }
 
     private CaseData.HistoryData[] loadHistoryData(String caseType) {
@@ -200,8 +206,96 @@ public class CaseLoader {
         return levelGroups;
     }
 
+    private CaseData.GUI loadGUI(ConfigurationSection caseSection) {
+        ConfigurationSection guiSection = caseSection.getConfigurationSection("Gui");
+
+        if(guiSection != null) {
+            int size = guiSection.getInt("Size", 45);
+            if(!isValidGuiSize(size)) {
+                size = 54;
+                logWarning("Wrong GUI size: " + size + ".Using 54");
+            }
+            ConfigurationSection items = guiSection.getConfigurationSection("Items");
+
+            if(items != null) {
+                Map<String, CaseData.GUI.Item> itemMap = loadGUIItems(items);
+                return new CaseData.GUI(size, itemMap);
+            }
+        }
+
+        return null;
+    }
+
+    @NotNull
+    private Map<String, CaseData.GUI.Item> loadGUIItems(@NotNull ConfigurationSection itemsSection) {
+        HashMap<String, CaseData.GUI.Item> itemMap = new HashMap<>();
+        for (String i : itemsSection.getKeys(false)) {
+            ConfigurationSection itemSection = itemsSection.getConfigurationSection(i);
+            if(itemSection != null) {
+                CaseData.GUI.Item item = loadGUIItem(i, itemSection);
+                itemMap.put(i, item);
+            }
+        }
+        return itemMap;
+    }
+
+
+    private CaseData.GUI.Item loadGUIItem(String i, @NotNull ConfigurationSection itemSection) {
+        String id = itemSection.getString("Material");
+        String displayName = itemSection.getString("DisplayName", "");
+        boolean enchanted = itemSection.getBoolean("Enchanted");
+        String itemType = itemSection.getString("Type", "DEFAULT");
+        List<String> lore = itemSection.getStringList("Lore");
+        int modelData = itemSection.getInt("ModelData", -1);
+        String[] rgb = loadRgb(itemSection, "Rgb");
+        List<Integer> slots = getItemSlots(itemSection);
+
+
+        CaseData.Item.Material material = new CaseData.Item.Material(id, null, displayName, enchanted, lore);
+
+        return new CaseData.GUI.Item(i, itemType, material, slots, modelData, rgb);
+    }
+
+
+    private List<Integer> getItemSlots(ConfigurationSection itemSection) {
+        if (itemSection.isList("Slots")) {
+            return getItemSlotsListed(itemSection);
+        } else {
+            return getItemSlotsRanged(itemSection);
+        }
+    }
+
+    private List<Integer> getItemSlotsListed(ConfigurationSection itemSection) {
+        List<Integer> slots = new ArrayList<>();
+        List<String> temp = itemSection.getStringList("Slots");
+        for (String slot : temp) {
+            String[] values = slot.split("-", 2);
+            if (values.length == 2) {
+                for (int i = Integer.parseInt(values[0]); i <= Integer.parseInt(values[1]); i++) {
+                    slots.add(i);
+                }
+            } else {
+                slots.add(Integer.parseInt(slot));
+            }
+        }
+        return slots;
+    }
+
+    private List<Integer> getItemSlotsRanged(ConfigurationSection itemSection) {
+        String[] slotArgs = itemSection.getString("Slots", "0-0").split("-");
+        int range1 = Integer.parseInt(slotArgs[0]);
+        int range2 = slotArgs.length >= 2 ? Integer.parseInt(slotArgs[1]) : range1;
+        return IntStream.rangeClosed(range1, range2).boxed().collect(Collectors.toList());
+    }
+
+
+
     private void logWarning(String message) {
         plugin.getLogger().warning(message);
+    }
+
+    private boolean isValidGuiSize(int size) {
+        return size >= 9 && size <= 54 && size % 9 == 0;
     }
 
 }
