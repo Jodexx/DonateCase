83d6c5ebe8fdd3156f2790d61b7089532abef277
Jodexx <0984134q@gmail.com>
2024-07-05 01:13:22 +0300

    New open_info mysql table

---
+++ src/main/java/com/jodexindustries/donatecase/database/sql/entities/OpenInfoTable.java
@@ -0,0 +1,38 @@
+package com.jodexindustries.donatecase.database.sql.entities;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "open_info")
+public class OpenInfoTable {
+    @DatabaseField(canBeNull = false)
+    private String player;
+    @DatabaseField(columnName = "case_type")
+    private String caseType;
+    @DatabaseField(defaultValue = "0")
+    private int count;
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public String getCaseType() {
+        return caseType;
+    }
+
+    public void setCaseType(String caseType) {
+        this.caseType = caseType;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
