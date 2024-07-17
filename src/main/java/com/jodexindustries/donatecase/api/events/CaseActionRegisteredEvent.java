dc7b2d9288671b663eabcd61d180145520acdd13
Jodexx <0984134q@gmail.com>
2024-07-17 20:04:15 +0300

    Add description for CaseActionRegisteredEvent

--- src/main/java/com/jodexindustries/donatecase/api/events/CaseActionRegisteredEvent.java
+++ src/main/java/com/jodexindustries/donatecase/api/events/CaseActionRegisteredEvent.java
@@ -5,6 +5,9 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Called when the case action is registered in DonateCase
+ */
 public class CaseActionRegisteredEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
     private final String caseActionName;
