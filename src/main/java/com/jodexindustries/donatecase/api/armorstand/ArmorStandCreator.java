33edddefd2b555ea60e33ac79dbf151da39e44bc
Jodexx <0984134q@gmail.com>
2024-07-22 00:15:43 +0300

    add new methods

--- src/main/java/com/jodexindustries/donatecase/api/armorstand/ArmorStandCreator.java
+++ src/main/java/com/jodexindustries/donatecase/api/armorstand/ArmorStandCreator.java
@@ -3,21 +3,31 @@ package com.jodexindustries.donatecase.api.armorstand;
 import org.bukkit.Location;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.Metadatable;
 import org.bukkit.util.EulerAngle;
 
-public interface ArmorStandCreator {
-    void spawnArmorStand(Location location);
+public interface ArmorStandCreator extends Metadatable {
     void setVisible(boolean isVisible);
     void setCustomName(String displayName);
     void teleport(Location location);
     @Deprecated
     void setHelmet(ItemStack item);
     void setEquipment(EquipmentSlot equipmentSlot, ItemStack item);
-    void setPose(ArmorStandEulerAngle angle);
+    void setAngle(ArmorStandEulerAngle angle);
     void setRotation(float yaw, float pitch);
     void setHeadPose(EulerAngle eulerAngle);
     void setGravity(boolean hasGravity);
     void setSmall(boolean small);
+    void setMarker(boolean marker);
+    void setCollidable(boolean collidable);
+    void setCustomNameVisible(boolean flag);
     Location getLocation();
+
+    /**
+     * Used for PacketArmorStandCreator
+     */
+    default void spawn() {}
+    default void updateMeta() {}
+
     void remove();
 }
