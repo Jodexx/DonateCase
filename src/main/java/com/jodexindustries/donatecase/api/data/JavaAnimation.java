ab1547cf319d4cc0640e03c0035764878a3fe65b
Jodexx <0984134q@gmail.com>
2024-07-27 20:38:46 +0300

    remove constructor

--- src/main/java/com/jodexindustries/donatecase/api/data/JavaAnimation.java
+++ src/main/java/com/jodexindustries/donatecase/api/data/JavaAnimation.java
@@ -6,21 +6,21 @@ import org.bukkit.entity.Player;
 import java.util.UUID;
 
 public abstract class JavaAnimation implements IAnimation {
-    private final Player player;
-    private final Location location;
-    private final UUID uuid;
-    private final CaseData caseData;
-    private final CaseData.Item winItem;
+    private Player player;
+    private Location location;
+    private UUID uuid;
+    private CaseData caseData;
+    private CaseData.Item winItem;
 
     /**
-     * Constructor for initializing
      * @param player   Player who opened case
      * @param uuid Active case uuid
      * @param location Case location
      * @param caseData Case data
      * @param winItem  winItem
      */
-    public JavaAnimation(Player player, Location location, UUID uuid, CaseData caseData, CaseData.Item winItem) {
+    public void init(Player player, Location location, UUID uuid, CaseData caseData,
+                     CaseData.Item winItem) {
         this.player = player;
         this.location = location;
         this.uuid = uuid;
