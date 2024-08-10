4d93756be2c0b04286ae25b3b835b86de1c114a7
Jodexx <0984134q@gmail.com>
2024-08-10 23:23:49 +0300

    Implement exists handlers

---
+++ src/main/java/com/jodexindustries/donatecase/api/impl/items/HISTORYItemHandlerImpl.java
@@ -0,0 +1,133 @@
+package com.jodexindustries.donatecase.api.impl.items;
+
+import com.jodexindustries.donatecase.api.Case;
+import com.jodexindustries.donatecase.api.GUITypedItemManager;
+import com.jodexindustries.donatecase.api.data.CaseData;
+import com.jodexindustries.donatecase.api.data.GUI;
+import com.jodexindustries.donatecase.api.data.gui.GUITypedItem;
+import com.jodexindustries.donatecase.api.data.gui.TypedItemHandler;
+import com.jodexindustries.donatecase.gui.CaseGui;
+import com.jodexindustries.donatecase.tools.Tools;
+import com.jodexindustries.donatecase.tools.Trio;
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class HISTORYItemHandlerImpl implements TypedItemHandler {
+
+    public HISTORYItemHandlerImpl(GUITypedItemManager manager) {
+        GUITypedItem item = manager.builder("HISTORY")
+                .handler(this)
+                .build();
+
+        manager.registerItem(item);
+    }
+
+    @NotNull
+    @Override
+    public GUI.Item handle(@NotNull CaseGui caseGui, @NotNull CaseData caseData, GUI.@NotNull Item item) {
+        CaseData.Item.Material material = item.getMaterial();
+
+        Trio<String, String, List<String>> trio = handleHistoryItem(caseData, item, caseGui.getGlobalHistoryData());
+        if (trio.getFirst() != null) material.setId(trio.getFirst());
+        if (trio.getSecond() != null) material.setDisplayName(trio.getSecond());
+        if (trio.getThird() != null) material.setLore(trio.getThird());
+
+        return item;
+    }
+
+    private Trio<String, String, List<String>> handleHistoryItem(CaseData caseData, GUI.Item item, List<CaseData.HistoryData> globalHistoryData) {
+        Trio<String, String, List<String>> trio = new Trio<>();
+
+        String caseType = caseData.getCaseType();
+
+        String[] typeArgs = item.getType().split("-");
+        int index = Integer.parseInt(typeArgs[1]);
+        caseType = (typeArgs.length >= 3) ? typeArgs[2] : caseType;
+        boolean isGlobal = caseType.equalsIgnoreCase("GLOBAL");
+
+        CaseData historyCaseData = isGlobal ? null : Case.getCase(caseType);
+        if (historyCaseData == null && !isGlobal) {
+            Case.getInstance().getLogger().warning("Case " + caseType + " HistoryData is null!");
+            return trio;
+        }
+
+        if (!isGlobal) historyCaseData = historyCaseData.clone();
+
+        CaseData.HistoryData data = getHistoryData(caseType, isGlobal, globalHistoryData, index, historyCaseData);
+        if (data == null) return trio;
+
+        if (isGlobal) historyCaseData = Case.getCase(data.getCaseType());
+        if (historyCaseData == null) return trio;
+
+        CaseData.Item historyItem = historyCaseData.getItem(data.getItem());
+        if (historyItem == null) return trio;
+        String material = item.getMaterial().getId();
+        if(material == null) material = "HEAD:" + data.getPlayerName();
+
+        if (material.equalsIgnoreCase("DEFAULT")) material = historyItem.getMaterial().getId();
+
+        String[] template = getTemplate(historyCaseData, data, historyItem);
+
+        String displayName = Tools.rt(item.getMaterial().getDisplayName(), template);
+        List<String> lore = Tools.rt(item.getMaterial().getLore(), template);
+
+        trio.setFirst(material);
+        trio.setSecond(displayName);
+        trio.setThird(lore);
+
+        return trio;
+    }
+
+    private String[] getTemplate(CaseData historyCaseData, CaseData.HistoryData data, CaseData.Item historyItem) {
+
+        DateFormat formatter = new SimpleDateFormat(Case.getConfig().getConfig().getString("DonateCase.DateFormat", "dd.MM HH:mm:ss"));
+        String dateFormatted = formatter.format(new Date(data.getTime()));
+        String group = data.getGroup();
+        String groupDisplayName = data.getItem() != null ? historyItem.getMaterial().getDisplayName() : "group_not_found";
+        String action = data.getAction() != null ? data.getAction() : group;
+
+        String randomActionDisplayName = "random_action_not_found";
+        if(data.getAction() != null && !data.getAction().isEmpty()) {
+            CaseData.Item.RandomAction randomAction = historyItem.getRandomAction(data.getAction());
+            if(randomAction != null) {
+                randomActionDisplayName = randomAction.getDisplayName();
+            }
+        } else {
+            randomActionDisplayName = groupDisplayName;
+        }
+
+        return new String[]{
+                "%action%:" + action,
+                "%actiondisplayname%:" + randomActionDisplayName,
+                "%casedisplayname%:" + historyCaseData.getCaseDisplayName(),
+                "%casename%:" + data.getCaseType(),
+                "%casetitle%:" + historyCaseData.getCaseTitle(),
+                "%time%:" + dateFormatted,
+                "%group%:" + group,
+                "%player%:" + data.getPlayerName(),
+                "%groupdisplayname%:" + groupDisplayName
+        };
+    }
+
+    private CaseData.HistoryData getHistoryData(String caseType, boolean isGlobal, List<CaseData.HistoryData> globalHistoryData, int index, CaseData historyCaseData) {
+        CaseData.HistoryData data = null;
+        if (isGlobal) {
+            if (globalHistoryData.size() <= index) return null;
+            data = globalHistoryData.get(index);
+        } else {
+            if (!Case.getInstance().sql) {
+                data = historyCaseData.getHistoryData()[index];
+            } else {
+                List<CaseData.HistoryData> dbData = Case.sortHistoryDataByCase(globalHistoryData, caseType);
+                if (!dbData.isEmpty() && dbData.size() > index) {
+                    data = dbData.get(index);
+                }
+            }
+        }
+        return data;
+    }
+}
---
+++ src/main/java/com/jodexindustries/donatecase/api/impl/items/OPENItemClickHandlerImpl.java
@@ -0,0 +1,57 @@
+package com.jodexindustries.donatecase.api.impl.items;
+
+import com.jodexindustries.donatecase.api.Case;
+import com.jodexindustries.donatecase.api.GUITypedItemManager;
+import com.jodexindustries.donatecase.api.data.CaseData;
+import com.jodexindustries.donatecase.api.data.gui.GUITypedItem;
+import com.jodexindustries.donatecase.api.data.gui.TypedItemClickHandler;
+import com.jodexindustries.donatecase.api.events.CaseGuiClickEvent;
+import com.jodexindustries.donatecase.api.events.OpenCaseEvent;
+import com.jodexindustries.donatecase.api.events.PreOpenCaseEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class OPENItemClickHandlerImpl implements TypedItemClickHandler {
+    public OPENItemClickHandlerImpl(GUITypedItemManager manager) {
+        GUITypedItem item = manager.builder("OPEN")
+                .click(this)
+                .build();
+
+        manager.registerItem(item);
+    }
+
+    @Override
+    public void onClick(@NotNull CaseGuiClickEvent e) {
+        Location location = e.getLocation();
+        String itemType = e.getItemType();
+        Player p = (Player) e.getWhoClicked();
+        String playerName = p.getName();
+        CaseData caseData = e.getCaseData();
+        String caseType = caseData.getCaseType();
+
+        if(itemType.contains("_")) {
+            String[] parts = itemType.split("_");
+            if(parts.length >= 2) caseType = parts[1];
+        }
+
+        PreOpenCaseEvent event = new PreOpenCaseEvent(p, caseType, location.getBlock());
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        if (!event.isCancelled()) {
+            if (Case.getKeys(caseType, playerName) >= 1) {
+                Case.removeKeys(caseType, playerName, 1);
+
+                OpenCaseEvent openEvent = new OpenCaseEvent(p, caseType, location.getBlock());
+                Bukkit.getServer().getPluginManager().callEvent(openEvent);
+
+                if (!openEvent.isCancelled())
+                    Case.getInstance().api.getAnimationManager().startAnimation(p, location, caseType);
+            } else {
+                Case.executeActions(p, caseData.getNoKeyActions());
+            }
+        }
+
+        p.closeInventory();
+    }
+}
