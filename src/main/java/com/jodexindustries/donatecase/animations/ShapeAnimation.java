d1bc580ce0fe891233854c29e0c46e86e31419ea
Jodexx <0984134q@gmail.com>
2024-08-24 19:54:54 +0300

    fix complexity (2)

--- src/main/java/com/jodexindustries/donatecase/animations/ShapeAnimation.java
+++ src/main/java/com/jodexindustries/donatecase/animations/ShapeAnimation.java
@@ -11,10 +11,11 @@ import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
 import java.util.function.Consumer;
 
 public class ShapeAnimation extends JavaAnimation {
@@ -23,6 +24,7 @@ public class ShapeAnimation extends JavaAnimation {
     public void start() {
         getLocation().add(0.5, -0.1, 0.5);
         getLocation().setYaw(-70.0F);
+
         final ArmorStandCreator as = Tools.createArmorStand(getLocation());
         boolean small = Case.getConfig().getAnimations().getBoolean("Shape.SmallArmorStand", true);
         as.setSmall(small);
@@ -30,8 +32,6 @@ public class ShapeAnimation extends JavaAnimation {
         as.setGravity(false);
         as.spawn();
 
-        float whiteSize = (float) Case.getConfig().getAnimations().getDouble("Shape.Particle.White.Size");
-        float orangeSize = (float) Case.getConfig().getAnimations().getDouble("Shape.Particle.Orange.Size");
         final String orangeRgbString = Case.getConfig().getAnimations().getString("Shape.Particle.Orange.Rgb");
         final String whiteRgbString = Case.getConfig().getAnimations().getString("Shape.Particle.White.Rgb");
 
@@ -39,7 +39,7 @@ public class ShapeAnimation extends JavaAnimation {
         final Color whiteColor = Tools.fromRGBString(whiteRgbString, Color.WHITE);
 
         Bukkit.getScheduler().runTaskTimer(Case.getInstance(),
-                new Task(as, whiteSize, orangeSize, orangeColor, whiteColor),
+                new Task(as, orangeColor, whiteColor),
                 0L, 2L);
     }
 
@@ -47,8 +47,10 @@ public class ShapeAnimation extends JavaAnimation {
 
         private int tick;
         private double tail;
-        private Location l;
-        
+        private final Location l;
+
+        @NotNull
+        private final World world;
         private final EquipmentSlot itemSlot;
         private final ArmorStandEulerAngle armorStandEulerAngle;
 
@@ -57,36 +59,31 @@ public class ShapeAnimation extends JavaAnimation {
         private final float orangeSize;
         private final Color orangeColor;
         private final Color whiteColor;
-        
-        public Task(final ArmorStandCreator as, final float whiteSize, final float orangeSize, final Color orangeColor, final Color whiteColor) {
+
+        public Task(final ArmorStandCreator as, final Color orangeColor, final Color whiteColor) {
             this.as = as;
-            this.whiteSize = whiteSize;
-            this.orangeSize = orangeSize;
+            this.l = as.getLocation();
+            this.whiteSize = (float) Case.getConfig().getAnimations().getDouble("Shape.Particle.White.Size");
+            this.orangeSize = (float) Case.getConfig().getAnimations().getDouble("Shape.Particle.Orange.Size");
             this.orangeColor = orangeColor;
             this.whiteColor = whiteColor;
             this.itemSlot = EquipmentSlot.valueOf(Case.getConfig().getAnimations().getString("Shape.ItemSlot", "HEAD")
                     .toUpperCase());
             this.armorStandEulerAngle = Tools.getArmorStandEulerAngle("Shape.Pose");
-
+            world = l.getWorld() != null ? l.getWorld() : getPlayer().getWorld();
         }
 
         @Override
         public void accept(BukkitTask task) {
-            if (tick == 0) {
-                l = as.getLocation();
-            }
-            if (tick >= 7) {
-                if (tick == 16) {
-                    if (getWinItem().getMaterial().getItemStack().getType() != Material.AIR) {
-                        as.setEquipment(itemSlot, getWinItem().getMaterial().getItemStack());
-                    }
-                    as.setAngle(armorStandEulerAngle);
-                    as.setCustomName(getWinItem().getMaterial().getDisplayName());
-                    as.updateMeta();
-                    Tools.launchFirework(l.clone().add(0.0, 0.8, 0.0));
-                    Case.animationPreEnd(getCaseData(), getPlayer(), getUuid(), getWinItem());
-
+            if (tick == 16) {
+                if (getWinItem().getMaterial().getItemStack().getType() != Material.AIR) {
+                    as.setEquipment(itemSlot, getWinItem().getMaterial().getItemStack());
                 }
+                as.setAngle(armorStandEulerAngle);
+                as.setCustomName(getWinItem().getMaterial().getDisplayName());
+                as.updateMeta();
+                Tools.launchFirework(l.clone().add(0.0, 0.8, 0.0));
+                Case.animationPreEnd(getCaseData(), getPlayer(), getUuid(), getWinItem());
             }
 
             if (tick <= 15) {
@@ -95,49 +92,54 @@ public class ShapeAnimation extends JavaAnimation {
                     as.setAngle(armorStandEulerAngle);
                     as.setEquipment(itemSlot, item.getMaterial().getItemStack());
                 }
+
                 String winGroupDisplayName = Tools.rc(Case.getInstance().papi.setPlaceholders(getPlayer(),
                         item.getMaterial().getDisplayName()));
-                if(item.getMaterial().getDisplayName() != null && !item.getMaterial().getDisplayName().isEmpty())
+                if (item.getMaterial().getDisplayName() != null && !item.getMaterial().getDisplayName().isEmpty()) {
                     as.setCustomNameVisible(true);
+                }
                 as.setCustomName(winGroupDisplayName);
                 as.updateMeta();
+
                 if (tick <= 8) {
                     Particle.DustOptions dustOptions = new Particle.DustOptions(orangeColor, orangeSize);
-                    Objects.requireNonNull(l.getWorld()).spawnParticle(Particle.REDSTONE, l.clone().add(0.0, 0.4, 0.0), 5, 0.3, 0.3, 0.3, 0.0, dustOptions);
+                    world.spawnParticle(Particle.REDSTONE, l.clone().add(0.0, 0.4, 0.0), 5, 0.3, 0.3, 0.3, 0.0, dustOptions);
                 }
             }
 
-            Location las = as.getLocation().clone();
+            Location las = as.getLocation();
             las.setYaw(las.getYaw() + 20.0F);
             as.teleport(las);
-            l = l.add(0.0, 0.13, 0.0);
+
+            l.add(0.0, 0.13, 0.0);
             if (tick <= 7) {
                 l.setYaw(las.getYaw());
                 as.teleport(l);
             }
-            //white trail
+
             if (tick <= 15) {
                 tail += 0.25;
-                Location loc = l.clone();
-                loc = loc.add(0.0, 0.5, 0.0);
+                Location loc = l.clone().add(0.0, 0.5, 0.0);
+
                 for (double phi = 0.0; phi <= 9.4; phi += 1) {
-                    final double x = 0.09 * (9.5 - tail * 2.5) * Math.cos(tail + phi);
-                    final double z = 0.09 * (9.5 - tail * 2.5) * Math.sin(tail + phi);
+                    double x = 0.09 * (9.5 - tail * 2.5) * Math.cos(tail + phi);
+                    double z = 0.09 * (9.5 - tail * 2.5) * Math.sin(tail + phi);
                     loc.add(x, 0.0, z);
-                    if (!Bukkit.getVersion().contains("1.12")) {
-                        Particle.DustOptions dustOptions = new Particle.DustOptions(whiteColor, whiteSize);
-                        Objects.requireNonNull(l.getWorld()).spawnParticle(Particle.REDSTONE, l.clone().add(0.0, 0.4, 0.0), 1, 0.1, 0.1, 0.1, 0.0, dustOptions);
-                    } else {
-                        Objects.requireNonNull(l.getWorld()).spawnParticle(Particle.FIREWORKS_SPARK, l.clone().add(0.0, 0.4, 0.0), 1, 0.1, 0.1, 0.1, 0.0);
-                    }
+                    Particle.DustOptions dustOptions = new Particle.DustOptions(whiteColor, whiteSize);
+
+                    world.spawnParticle(
+                            Bukkit.getVersion().contains("1.12") ? Particle.FIREWORKS_SPARK : Particle.REDSTONE,
+                            loc.clone().add(0.0, 0.4, 0.0),
+                            1, 0.1, 0.1, 0.1, 0.0, dustOptions
+                    );
                     loc.subtract(x, 0.0, z);
-                    if (tail >= 22) {
-                        loc.add(x, 0.0, z);
-                        tail = 0.0;
-                    }
+                }
+
+                if (tail >= 22) {
+                    tail = 0.0;
                 }
             }
-            //end
+
             if (tick >= 40) {
                 as.remove();
                 task.cancel();
@@ -146,5 +148,6 @@ public class ShapeAnimation extends JavaAnimation {
 
             ++tick;
         }
+
     }
-    }
+}
