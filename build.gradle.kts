b32b08563c4601e97028f46ecc6b3087caf061ed
Jodexx <0984134q@gmail.com>
2025-01-08 16:41:28 +0100

    Remove unused task

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,20 +29,3 @@ subprojects {
         options.compilerArgs.add("-Xdoclint:none")
     }
 }
-
-val apiVersion: String = properties["api"].toString()
-
-tasks.register("replaceVersionInREADME") {
-    doLast {
-        ant.withGroovyBuilder {
-            // Replace version in XML-like tags
-            "replaceregexp"("match" to "<version>(listOf(0-9\\.)+)</version>", "replace" to "<version>$apiVersion</version>", "flags" to "g", "byline" to true) {
-                "fileset"("dir" to ".", "includes" to "README.md")
-            }
-            // Replace version in Gradle-like assignment
-            "replaceregexp"("match" to "com\\.jodexindustries\\.donatecase\\ =DonateCaseAPI\\ =(listOf(0-9\\.)+)", "replace" to "com.jodexindustries.donatecase =DonateCaseAPI =$apiVersion", "flags" to "g", "byline" to true) {
-                "fileset"("dir" to ".", "includes" to "README.md")
-            }
-        }
-    }
-}
