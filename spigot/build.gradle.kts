979969a18c6541d75bf4a428e3b066a580d27079
Jodexx <0984134q@gmail.com>
2025-01-24 18:02:28 +0100

    Remove junit test

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -30,9 +30,6 @@ dependencies {
     implementation(project(":api:spigot-api"))
     implementation(project(":common"))
 
-    testImplementation(platform("org.junit:junit-bom:5.11.4"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
