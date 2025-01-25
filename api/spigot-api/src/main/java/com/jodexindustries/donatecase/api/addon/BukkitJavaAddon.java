cb89ed87aa7d0df480ea0ebbd24fa1c9132923d7
Jodexx <0984134q@gmail.com>
2025-01-25 20:43:38 +0100

    Remove unused

--- api/spigot-api/src/main/java/com/jodexindustries/donatecase/api/addon/BukkitJavaAddon.java
+++ api/spigot-api/src/main/java/com/jodexindustries/donatecase/api/addon/BukkitJavaAddon.java
@@ -1,15 +1,16 @@
-package com.jodexindustries.donatecase.api.addon.external;
+package com.jodexindustries.donatecase.api.addon;
 
+import com.jodexindustries.donatecase.api.addon.Addon;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.logging.Logger;
 
-public class ExternalJavaAddon implements ExternalAddon {
+public class BukkitJavaAddon implements Addon {
     private final Plugin plugin;
 
-    public ExternalJavaAddon(Plugin plugin) {
+    public BukkitJavaAddon(Plugin plugin) {
         this.plugin = plugin;
     }
 
@@ -21,11 +22,6 @@ public class ExternalJavaAddon implements ExternalAddon {
     @Override
     public String getVersion() {
         return plugin.getDescription().getVersion();
-    }
-
-    @Override
-    public Plugin getPlugin() {
-        return plugin;
     }
 
     @Override
