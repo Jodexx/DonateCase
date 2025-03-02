ab6175c46740dd254c38d2e763f497032836db4a
Jodexx <0984134q@gmail.com>
2025-03-02 15:49:11 +0100

    Refactor DCHistoryEditor addon

--- addons/DCHistoryEditor/src/main/java/com/jodexindustries/dchistoryeditor/MainCommand.java
+++ addons/DCHistoryEditor/src/main/java/com/jodexindustries/dchistoryeditor/MainCommand.java
@@ -1,25 +1,23 @@
-package com.jodexindustries.dchistoryeditor.commands;
+package com.jodexindustries.dchistoryeditor;
 
-import com.jodexindustries.donatecase.api.DCAPIBukkit;
 import com.jodexindustries.donatecase.api.data.casedata.CaseData;
 import com.jodexindustries.donatecase.api.data.database.DatabaseStatus;
 import com.jodexindustries.donatecase.api.data.subcommand.SubCommandExecutor;
 import com.jodexindustries.donatecase.api.data.subcommand.SubCommandTabCompleter;
 import com.jodexindustries.donatecase.api.database.CaseDatabase;
 import com.jodexindustries.donatecase.api.platform.DCCommandSender;
-import com.jodexindustries.donatecase.tools.DCToolsBukkit;
-import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import static com.jodexindustries.donatecase.api.tools.DCTools.rc;
+
 public class MainCommand implements SubCommandExecutor, SubCommandTabCompleter {
 
     private final CaseDatabase database;
-    private final DCAPIBukkit api;
+    private final MainAddon addon;
 
     private static final List<String> params = new ArrayList<>();
 
@@ -31,26 +29,27 @@ public class MainCommand implements SubCommandExecutor, SubCommandTabCompleter {
         params.add("action");
     }
 
-    public MainCommand(DCAPIBukkit api) {
-        this.api = api;
-        this.database = api.getDatabase();
+    public MainCommand(MainAddon addon) {
+        this.addon = addon;
+        this.database = addon.api.getDatabase();
     }
 
     @Override
-    public void execute(@NotNull DCCommandSender sender, @NotNull String label, @NotNull String[] args) {
-        Bukkit.getScheduler().runTaskAsynchronously(api.getDonateCase(), () -> {
+    public boolean execute(@NotNull DCCommandSender sender, @NotNull String label, @NotNull String[] args) {
+        addon.api.getPlatform().getScheduler().async(addon, () -> {
+
             if (args.length < 3) {
-                sender.sendMessage(DCToolsBukkit.rc("&c/dc historyeditor (remove/set) (casetype) (index/all) [param] [value]"));
+                sender.sendMessage(rc("&c/dc historyeditor (remove/set) (casetype) (index/all) [param] [value]"));
                 return;
             }
 
             String action = args[0].toLowerCase();
             String caseType = args[1];
 
-            CaseDataBukkit caseData = api.getCaseManager().get(caseType);
+            CaseData caseData = addon.api.getCaseManager().get(caseType);
 
             if(caseData == null) {
-                sender.sendMessage(DCToolsBukkit.rc("&cCase with type: " + caseType + " not found!"));
+                sender.sendMessage(rc("&cCase with type: " + caseType + " not found!"));
                 return;
             }
 
@@ -66,10 +65,12 @@ public class MainCommand implements SubCommandExecutor, SubCommandTabCompleter {
                 }
 
                 default: {
-                    sender.sendMessage(DCToolsBukkit.rc("&c/dc historyeditor (remove/set) (casetype) (index) [param] [value]"));
+                    sender.sendMessage(rc("&c/dc historyeditor (remove/set) (casetype) (index) [param] [value]"));
                 }
             }
-        });
+        }, 0L);
+
+        return true;
     }
 
     @Override
@@ -81,7 +82,7 @@ public class MainCommand implements SubCommandExecutor, SubCommandTabCompleter {
         }
 
         if(args.length == 2) {
-            list.addAll(api.getCaseManager().getMap().keySet());
+            list.addAll(addon.api.getCaseManager().getMap().keySet());
         }
 
         if(args.length == 3) {
@@ -96,30 +97,28 @@ public class MainCommand implements SubCommandExecutor, SubCommandTabCompleter {
         return list;
     }
 
-    private void handleRemove(CommandSender sender, CaseDataBukkit caseData, String arg) {
+    private void handleRemove(DCCommandSender sender, CaseData caseData, String arg) {
         if(arg.equalsIgnoreCase("all") ) {
-            database.removeHistoryData(caseData.getCaseType()).thenAccept(status -> {
+            database.removeHistoryData(caseData.caseType()).thenAccept(status -> {
                 removeInform(sender, status);
-                if(status == DatabaseStatus.COMPLETE) caseData.setHistoryData(new CaseData.History[caseData.getHistoryData().length]);
             });
         } else {
             int index = getIndex(sender, arg);
             if(index == -1) return;
 
-            database.removeHistoryData(caseData.getCaseType(), index).thenAccept(status -> {
+            database.removeHistoryData(caseData.caseType(), index).thenAccept(status -> {
                 removeInform(sender, status);
-                if(status == DatabaseStatus.COMPLETE) caseData.getHistoryData()[index] = null;
             });
         }
     }
 
-    private void handleSet(CommandSender sender, CaseData caseData, String[] args) {
+    private void handleSet(DCCommandSender sender, CaseData caseData, String[] args) {
         int index = getIndex(sender, args[2]);
         if(index == -1) return;
 
         if (args.length < 5) {
             // usage
-            sender.sendMessage(DCToolsBukkit.rc("&c/dc historyeditor set (casetype) (index) (value) (param)"));
+            sender.sendMessage(rc("&c/dc historyeditor set (casetype) (index) (value) (param)"));
             return;
         }
 
@@ -128,47 +127,46 @@ public class MainCommand implements SubCommandExecutor, SubCommandTabCompleter {
 
         if (!params.contains(param)) {
             // param is not found
-            sender.sendMessage(DCToolsBukkit.rc("&cParam " + param + " is not found!"));
+            sender.sendMessage(rc("&cParam " + param + " is not found!"));
             return;
         }
 
-        CaseData.History tempHistory = getHistoryData(caseData.getCaseType(), index);
+        CaseData.History tempHistory = getHistoryData(caseData.caseType(), index);
 
-        CaseData.History history = tempHistory == null ? new CaseData.History(null, caseData.getCaseType(), null, index, null, null) : tempHistory;
+        CaseData.History history = tempHistory == null ? new CaseData.History(null, caseData.caseType(), null, index, null, null) : tempHistory;
 
         switch (param) {
             case "item": {
-                history.setItem(value);
+                history.item(value);
                 break;
             }
 
             case "playername": {
-                history.setPlayerName(value);
+                history.playerName(value);
                 break;
             }
 
             case "time": {
-                history.setTime(Long.parseLong(value));
+                history.time(Long.parseLong(value));
                 break;
             }
 
             case "group": {
-                history.setGroup(value);
+                history.group(value);
                 break;
             }
 
             case "action": {
-                history.setAction(value);
+                history.action(value);
                 break;
             }
         }
 
-        database.setHistoryData(caseData.getCaseType(), index, history).thenAccept(status -> {
+        database.setHistoryData(caseData.caseType(), index, history).thenAccept(status -> {
             if (status == DatabaseStatus.COMPLETE) {
-                caseData.getHistoryData()[index] = history;
-                sender.sendMessage(DCToolsBukkit.rc("&aHistory data updated!"));
+                sender.sendMessage(rc("&aHistory data updated!"));
             } else {
-                sender.sendMessage(DCToolsBukkit.rc("&cError with history data updating!"));
+                sender.sendMessage(rc("&cError with history data updating!"));
             }
         });
     }
@@ -176,18 +174,18 @@ public class MainCommand implements SubCommandExecutor, SubCommandTabCompleter {
     private CaseData.History getHistoryData(String caseType, int index) {
         List<CaseData.History> histories = database.getHistoryData(caseType).join();
 
-        return histories.stream().filter(history -> history.getId() == index).findFirst().orElse(null);
+        return histories.stream().filter(history -> history.id() == index).findFirst().orElse(null);
     }
 
-    private void removeInform(CommandSender sender, DatabaseStatus status) {
-        sender.sendMessage(status == DatabaseStatus.COMPLETE ? DCToolsBukkit.rc("&aHistory data removed!") :
-                DCToolsBukkit.rc("&cError with history data removing!"));
+    private void removeInform(DCCommandSender sender, DatabaseStatus status) {
+        sender.sendMessage(status == DatabaseStatus.COMPLETE ? rc("&aHistory data removed!") :
+                rc("&cError with history data removing!"));
     }
 
-    private int getIndex(CommandSender sender, String string) {
+    private int getIndex(DCCommandSender sender, String string) {
         int index = parseInt(string);
         if (index <= -1 || index > 10) {
-            sender.sendMessage(DCToolsBukkit.rc("&cNumber format exception!"));
+            sender.sendMessage(rc("&cNumber format exception!"));
             return -1;
         }
         return index;
