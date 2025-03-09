53fe3c7f9a10c2b8b41fc3fad45f52a83bac8b83
Jodexx <0984134q@gmail.com>
2025-03-09 17:18:16 +0100

    DCEventManager - Conditions for events

--- addons/DCEventManager/src/main/java/com/jodexindustries/dceventmanager/data/EventData.java
+++ addons/DCEventManager/src/main/java/com/jodexindustries/dceventmanager/data/EventData.java
@@ -1,6 +1,9 @@
 package com.jodexindustries.dceventmanager.data;
 
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.configurate.ConfigurationNode;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Setting;
 
@@ -20,9 +23,94 @@ public class EventData {
         @Setting
         private String name;
 
-        // TODO Conditions
+        @Setting
+        private List<Condition> conditions;
 
         @Setting
         private List<String> actions;
+    }
+
+    @Getter
+    @ConfigSerializable
+    public static class Condition {
+
+        @Setting
+        private Type type;
+
+        @Setting
+        private String placeholder;
+
+        @Setting
+        private ConfigurationNode input;
+
+        public boolean compare(@Nullable Object output) {
+            Object input = this.input.raw();
+            if (input == null || output == null) return false;
+
+            return type.compare(input, output);
+        }
+
+        public enum Type {
+            /**
+             * =
+             */
+            EQUALS(0),
+
+            /**
+             * !=
+             */
+            NOT_EQUAL_TO(1),
+
+            /**
+             * <
+             */
+            LESS_THAN(2),
+
+            /**
+             * <=
+             */
+            LESS_THAN_OR_EQUAL_TO(3),
+
+            /**
+             * >
+             */
+            GREATER_THAN(4),
+
+            /**
+             * >=
+             */
+            GREATER_THAN_OR_EQUAL_TO(5);
+
+            public final int operation;
+
+            Type(int operation) {
+                this.operation = operation;
+            }
+
+            public boolean compare(@NotNull Object a, @NotNull Object b) {
+                if (a instanceof Number && b instanceof Number) {
+                    double numA = ((Number) a).doubleValue();
+                    double numB = ((Number) b).doubleValue();
+
+                    switch (operation) {
+                        case 0: return numA == numB;
+                        case 1: return numA != numB;
+                        case 2: return numA < numB;
+                        case 3: return numA <= numB;
+                        case 4: return numA > numB;
+                        case 5: return numA >= numB;
+                    }
+                }
+
+                switch (operation) {
+                    case 0: return a.equals(b);
+                    case 1: return !a.equals(b);
+                }
+
+                return false;
+            }
+
+
+        }
     }
 }
--- addons/DCEventManager/src/main/java/com/jodexindustries/dceventmanager/event/DCEventExecutor.java
+++ addons/DCEventManager/src/main/java/com/jodexindustries/dceventmanager/event/DCEventExecutor.java
@@ -10,9 +10,7 @@ import com.jodexindustries.donatecase.api.platform.DCPlayer;
 import net.kyori.event.EventSubscriber;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Level;
 
 import static com.jodexindustries.donatecase.api.tools.DCTools.rc;
@@ -39,50 +37,62 @@ public class DCEventExecutor implements EventSubscriber<DCEvent> {
         EventData data = tools.getConfigManager().getEventConfig().getEvents().get(name);
         if (data == null) return;
 
+        Map<String, Object> placeholders = getPlaceholders(event);
+
         for (EventData.Executor executor : data.getExecutors()) {
-            executeActions(event, replaceList(executor.getActions(), getPlaceholders(event)
-            ));
+            try {
+                if (checkConditions(executor.getConditions(), placeholders)) {
+                    executeActions(event, replaceList(executor.getActions(), placeholders));
+                }
+            } catch (Exception e) {
+                tools.main.getLogger().log(Level.WARNING, "Error with executing executor: " + executor.getName(), e);
+            }
         }
     }
 
-    private String[] getPlaceholders(DCEvent event) {
-        Map<String, EventPlaceholder> map = tools.getConfigManager().getPlaceholderConfig().getEventPlaceholders();
+    private boolean checkConditions(List<EventData.Condition> conditions, Map<String, Object> placeholders) {
+        for (EventData.Condition condition : conditions) {
+            Object output = placeholders.get(condition.getPlaceholder());
+            if(!condition.compare(output)) return false;
+        }
 
-        EventPlaceholder eventPlaceholder = map.get(name);
-        if (eventPlaceholder == null) return new String[0];
+        return true;
+    }
+
+    private Map<String, Object> getPlaceholders(DCEvent event) {
+        Map<String, Object> map = new HashMap<>();
+
+        Map<String, EventPlaceholder> placeholderMap = tools.getConfigManager().getPlaceholderConfig().getEventPlaceholders();
+
+        EventPlaceholder eventPlaceholder = placeholderMap.get(name);
+        if (eventPlaceholder == null) return map;
 
         List<EventPlaceholder.Placeholder> placeholders = eventPlaceholder.getPlaceholders();
 
-        String[] values = new String[placeholders.size() * 2];
-
-        int index = 0;
         for (EventPlaceholder.Placeholder placeholder : placeholders) {
-            values[index] = placeholder.getReplace();
+            if (map.containsKey(placeholder.getReplace())) continue;
 
-            String value = null;
+            Object value = null;
             try {
-                value = String.valueOf(Reflection.invokeMethodChain(event, placeholder.getMethod()));
+                value = Reflection.invokeMethodChain(event, placeholder.getMethod());
             } catch (Exception ex) {
                 tools.main.getLogger().log(Level.WARNING, "Failed to parse placeholder: " + placeholder.getName(), ex);
             }
 
-            values[index + 1] = value;
-
-            index += 2;
+            map.put(placeholder.getReplace(), value);
         }
-        return values;
+        return map;
     }
 
     private void executeActions(DCEvent event, List<String> actions) {
         DCPlayer player;
 
-        // TODO Make the field player in placeholders.yml for selecting correct method for player
-        // TODO Make the scanner for DCPlayer field searching to placeholders autocompleting
+        // TODO Make the scanner for all fields searching to placeholders autocompleting
         player = Reflection.getVar(event, "player", DCPlayer.class);
         if (player == null) player = Reflection.getVar(event, "getWhoClicked", DCPlayer.class);
 
         // DonateCase actions
-        if (player != null) tools.api.getActionManager().execute(player, actions);
+        tools.api.getActionManager().execute(player, actions);
 
         // DCEventManager actions
         for (String action : actions) {
@@ -96,16 +106,13 @@ public class DCEventExecutor implements EventSubscriber<DCEvent> {
         }
     }
 
-    public List<String> replaceList(List<String> list, String... args) {
+    public List<String> replaceList(List<String> list, Map<String, Object> map) {
         List<String> newList = new ArrayList<>();
         if (list == null) return newList;
 
         for (String line : list) {
-            for (int i = 0; i + 1 < args.length; i += 2) {
-                String repl = args[i + 1];
-                if (repl != null) {
-                    line = line.replace(args[i], repl);
-                }
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                line = line.replace(entry.getKey(), String.valueOf(entry.getValue()));
             }
             newList.add(rc(line));
         }
