5590e802bffb2841b1e0092c4f34559f6b962b9a
Jodexx <0984134q@gmail.com>
2025-04-08 18:44:22 +0200

    Add more facings and pitch

--- spigot/src/main/java/com/jodexindustries/donatecase/spigot/animations/Facing.java
+++ spigot/src/main/java/com/jodexindustries/donatecase/spigot/animations/Facing.java
@@ -1,14 +1,36 @@
 package com.jodexindustries.donatecase.spigot.animations;
 
 public enum Facing {
-    WEST(90),
-    NORTH(180),
-    EAST(270),
-    SOUTH(0);
+    // Horizontal
+    NORTH(180, 0),
+    NORTH_EAST(225, 0),
+    EAST(270, 0),
+    SOUTH_EAST(315, 0),
+    SOUTH(0, 0),
+    SOUTH_WEST(45, 0),
+    WEST(90, 0),
+    NORTH_WEST(135, 0),
+
+    // Vertical
+    UP(0, -90),
+    DOWN(0, 90),
+
+    // Vertical Diagonals
+    UP_NORTH(180, -45),
+    UP_EAST(270, -45),
+    UP_SOUTH(0, -45),
+    UP_WEST(90, -45),
+
+    DOWN_NORTH(180, 45),
+    DOWN_EAST(270, 45),
+    DOWN_SOUTH(0, 45),
+    DOWN_WEST(90, 45);
 
     public final float yaw;
+    public final float pitch;
 
-    Facing(float yaw) {
+    Facing(float yaw, float pitch) {
         this.yaw = yaw;
+        this.pitch = pitch;
     }
 }
