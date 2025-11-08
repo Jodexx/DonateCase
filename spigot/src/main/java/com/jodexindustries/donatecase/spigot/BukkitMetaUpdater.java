aa54631848437388fbb14f77afdc5a498ccd8bc9
Jodexx <0984134q@gmail.com>
2025-11-08 17:35:20 +0100

    improve item meta update logic and add color handling. See #80

--- spigot/src/main/java/com/jodexindustries/donatecase/spigot/BukkitMetaUpdater.java
+++ spigot/src/main/java/com/jodexindustries/donatecase/spigot/BukkitMetaUpdater.java
@@ -8,40 +8,48 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 public class BukkitMetaUpdater implements MetaUpdater {
 
     @Override
-    public void updateMeta(Object itemStack, String displayName, List<String> lore, int modelData,
-                           boolean enchanted, String[] rgb) {
-        if (itemStack instanceof ItemStack) {
-            ItemStack item = (ItemStack) itemStack;
-            ItemMeta meta = item.getItemMeta();
-            if (meta != null) {
-                if (displayName != null) meta.setDisplayName(DCTools.rc(displayName));
-                if (lore != null) meta.setLore(DCTools.rc(lore));
-                if (modelData != -1) meta.setCustomModelData(modelData);
-                if (enchanted) meta.addEnchant(Enchantment.LURE, 1, true);
+    public void updateMeta(Object itemStack, String displayName, List<String> lore, int modelData, boolean enchanted, String[] rgb) {
+        if (!(itemStack instanceof ItemStack)) {
+            return;
+        }
 
-                if (rgb != null && rgb.length >= 3 && meta instanceof LeatherArmorMeta) {
-                    LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) meta;
-                    leatherArmorMeta.setColor(DCToolsBukkit.fromRGBString(rgb, Color.WHITE));
-                }
+        ItemStack item = (ItemStack) itemStack;
+        ItemMeta meta = item.getItemMeta();
+        if (meta != null) {
+            if (displayName != null) meta.setDisplayName(DCTools.rc(displayName));
+            if (lore != null) meta.setLore(DCTools.rc(lore));
+            if (modelData != -1) meta.setCustomModelData(modelData);
+            if (enchanted) meta.addEnchant(Enchantment.LURE, 1, true);
 
-                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-                meta.addItemFlags(ItemFlag.HIDE_DYE);
-                try {
-                    meta.addItemFlags(ItemFlag.valueOf("HIDE_POTION_EFFECTS"));
-                } catch (IllegalArgumentException ignored) {
-                }
-
-                item.setItemMeta(meta);
+            if (rgb != null && rgb.length >= 3) {
+                Color color = DCToolsBukkit.fromRGBString(rgb, Color.WHITE);
+                updateColor(meta, color);
             }
+
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            meta.addItemFlags(ItemFlag.HIDE_DYE);
+            try {
+                meta.addItemFlags(ItemFlag.valueOf("HIDE_POTION_EFFECTS"));
+            } catch (IllegalArgumentException ignored) {
+            }
+
+            item.setItemMeta(meta);
         }
     }
 
+    private void updateColor(ItemMeta meta, Color color) {
+        try {
+            Method setColorMethod = meta.getClass().getDeclaredMethod("setColor", Color.class);
+            setColorMethod.invoke(meta, color);
+        } catch (Exception ignored) {
+        }
+    }
 }
